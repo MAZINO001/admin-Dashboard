cad70bb00bb36d7b5fb15e86e5881016b36ce964
mounir <marnissimounir05@gmail.com>
2024-03-10 20:18:00 +0100

    adding themes

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -2,10 +2,19 @@ import { ColorModeContext, useMode } from "./Theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 
 function App() {
-  
-  return <div className="app">
-
-  </div>;
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
